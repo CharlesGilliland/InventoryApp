e0456bc3a7aca2eef7dd42c4f98c845a2725b872
Charlie Gilliland <charliegilliland@hotmail.co.uk>
2021-07-26 18:00:57 +0100

    Adding login view and view model

---
+++ InventoryApp/Commands/ShowLoginRegisterFocusCommand.cs
@@ -0,0 +1,20 @@
+﻿using InventoryApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryApp.Commands
+{
+    public class ShowLoginRegisterFocusCommand : CommandBase
+    {
+        LoginViewModel viewModel;
+        public ShowLoginRegisterFocusCommand(LoginViewModel vm)
+        {
+            viewModel = vm;
+        }
+        public override void Execute(object parameter)
+        {
+            viewModel.ChangeFocus();
+        }
+    }
+}
---
+++ InventoryApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,112 @@
+﻿using InventoryApp.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using dotenv.net;
+
+namespace InventoryApp.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private Visibility loginVis;
+        public Visibility LoginVis 
+        {
+            get { return loginVis; }
+            set 
+            { 
+                loginVis = value;
+                OnPropertyChanged("LoginVis");
+            } 
+        }
+
+        private Visibility registerVis;
+        public Visibility RegisterVis
+        {
+            get { return registerVis; }
+            set 
+            { 
+                registerVis = value;
+                OnPropertyChanged("RegisterVis");
+            }
+        }
+        private string buttonContent;
+        public string ButtonContent
+        {
+            get { return buttonContent; }
+            set 
+            { 
+                buttonContent = value;
+                OnPropertyChanged("ButtonContent");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set 
+            { 
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set 
+            { 
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private string confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return confirmPassword; }
+            set 
+            { 
+                confirmPassword = value;
+                OnPropertyChanged("ConfirmPassword");
+            }
+        }
+
+        public ShowLoginRegisterFocusCommand ShowLoginRegisterFocusCommand { get; set; }
+
+
+
+        public LoginViewModel()
+        {
+            ShowLoginRegisterFocusCommand = new ShowLoginRegisterFocusCommand(this);
+
+            LoginVis = Visibility.Visible;
+            RegisterVis = Visibility.Collapsed;
+            ButtonContent = "Register";
+
+            DotEnv.Load();
+            var envVars = DotEnv.Read();
+        }
+
+
+        public void ChangeFocus()
+        {
+            if(LoginVis == Visibility.Visible)
+            {
+                LoginVis = Visibility.Collapsed;
+                RegisterVis = Visibility.Visible;
+                ButtonContent = "Login";
+            }
+            else
+            {
+                LoginVis = Visibility.Visible;
+                RegisterVis = Visibility.Collapsed;
+                ButtonContent = "Register";
+            }
+        }
+    }
+}
